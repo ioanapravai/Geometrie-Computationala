b0262af08b47563f8b42d2d3c7023e284c9af0b0
ioanapravai <ioana.pravai99@gmail.com>
2020-02-11 19:33:56 +0200

    Intersectii Segmente

---
+++ Intersectii_Segmente/cmake-build-debug/CMakeFiles/Intersectii_Segmente.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Intersectii_Segmente.dir/main.cpp.obj"
+  "Intersectii_Segmente.exe"
+  "Intersectii_Segmente.exe.manifest"
+  "Intersectii_Segmente.pdb"
+  "libIntersectii_Segmente.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Intersectii_Segmente.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
